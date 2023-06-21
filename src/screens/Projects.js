5a5d6f27978293da673d76c31e9989e568d75c2f
Agung1606 <agngsptra928@gmail.com>
2023-06-21 15:12:12 +0700

    moved CardTask to Projects.js screen

--- src/screens/Projects.js
+++ src/screens/Projects.js
@@ -1,12 +1,42 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, FlatList, TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import React from "react";
+
+import { FontAwesome5 } from "@expo/vector-icons";
+
+import { TASKS } from "../constant";
+import { CardTask } from "../components";
 
 const Projects = () => {
   return (
-    <View>
-      <Text>Projects</Text>
-    </View>
-  )
-}
+    <SafeAreaView className="flex-1 mx-2">
+      <View className="mb-4 py-4 flex-row space-x-6 bg-slate-200/80 rounded-b-2xl">
+        <TouchableOpacity className="bg-blue-500 flex-row items-center space-x-6 p-4 rounded-2xl">
+          <FontAwesome5 name="clipboard-list" size={22} color={"#FFF"} />
+          <Text className="font-InterSemiBold text-white">To do</Text>
+        </TouchableOpacity>
+        <TouchableOpacity className="bg-white flex-row items-center space-x-6 p-4 rounded-2xl">
+          <FontAwesome5 name="clipboard-list" size={22} color={"#3b82f6"} />
+          <Text className="font-InterSemiBold">In progress</Text>
+        </TouchableOpacity>
+      </View>
+      <FlatList
+        data={TASKS}
+        renderItem={({ item }) => (
+          <CardTask
+            key={item.id}
+            task={item.task}
+            level={item.level}
+            desc={item.desc}
+            time={item.time}
+            day={item.day}
+          />
+        )}
+        showsVerticalScrollIndicator={false}
+        keyExtractor={(item) => item.id}
+      />
+    </SafeAreaView>
+  );
+};
 
-export default Projects
+export default Projects;
--- src/screens/Tasks.js
+++ src/screens/Tasks.js
@@ -2,18 +2,17 @@ import React from "react";
 import { View, ScrollView, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-import { assets, REMINDER, INFOTASKS, TASKS } from "../constant";
+import { assets, REMINDER, INFOTASKS } from "../constant";
 import {
   TaskHeader,
   CardRemainder,
   CardInfoTask,
-  CardTask,
 } from "../components";
 
 const Tasks = () => {
   return (
-    <SafeAreaView className="flex-1 mx-2">
-      <ScrollView showsVerticalScrollIndicator={false}>
+    <SafeAreaView className="flex-1">
+      <ScrollView showsVerticalScrollIndicator={false} className="mx-2">
         <TaskHeader title="Tasker" imgUri={assets.defaultProfile} />
         <View className="flex-row justify-center items-center flex-wrap mb-6">
           {REMINDER.map((task) => (
@@ -31,16 +30,6 @@ const Tasks = () => {
             dotColor={item.dotColor}
             status={item.status}
             numsTask={item.numsTask}
-          />
-        ))}
-        {TASKS.map((item) => (
-          <CardTask
-            key={item.id}
-            task={item.task}
-            level={item.level}
-            desc={item.desc}
-            time={item.time}
-            day={item.day}
           />
         ))}
       </ScrollView>
